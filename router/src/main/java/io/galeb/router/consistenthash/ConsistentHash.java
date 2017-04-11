c6a248f751e126bed24351e24505c836d82617e1
Marcelo Monteiro (tuxmonteiro) <marcelotmonteiro@gmail.com>
2017-04-11 15:08:37 -0300

    added Consistent Hash support

---
+++ router/src/main/java/io/galeb/router/consistenthash/ConsistentHash.java
@@ -0,0 +1,103 @@
+/**
+ *
+ */
+
+package io.galeb.router.consistenthash;
+
+import java.util.Collection;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+public class ConsistentHash<T> {
+
+    /** The {@link HashAlgorithm Hash Algorithm} */
+    private HashAlgorithm               hashAlgorithm;
+
+    /** The number of replicas. */
+    private int                         numberOfReplicas;
+
+    /** The circle. */
+    private final SortedMap<Integer, T> circle  = new TreeMap<>();
+
+    /**
+     * Instantiates a new consistent hash.
+     *
+     * @param hashAlgorithm The {@link HashAlgorithm Hash Algorithm}
+     * @param numberOfReplicas the number of replicas
+     * @param nodes nodes collection
+     */
+    public ConsistentHash(HashAlgorithm hashAlgorithm, int numberOfReplicas,
+            Collection<T> nodes) {
+        this.hashAlgorithm = hashAlgorithm;
+        this.numberOfReplicas = numberOfReplicas;
+
+        for (T node : nodes) {
+            add(node);
+        }
+    }
+
+    /**
+     * Adds a node.
+     *
+     * @param node a node
+     */
+    public void add(T node) {
+        for (int i = 0; i < numberOfReplicas; i++) {
+            circle.put(hashAlgorithm.hash(node.toString() + i).asInt(), node);
+        }
+    }
+
+    /**
+     * Removes a node.
+     *
+     * @param node a node
+     */
+    public void remove(T node) {
+        for (int i = 0; i < numberOfReplicas; i++) {
+            circle.remove(hashAlgorithm.hash(node.toString() + i).asInt());
+        }
+    }
+
+    /**
+     * Gets a node.
+     *
+     * @param key a key
+     * @return a node from cicle
+     */
+    public T get(String key) {
+        if (circle.isEmpty()) {
+            return null;
+        }
+
+        int hash= hashAlgorithm.hash(key).asInt();
+
+        if (!circle.containsKey(hash)) {
+            SortedMap<Integer, T> tailMap = circle.tailMap(hash);
+            hash = tailMap.isEmpty() ? circle.firstKey() : tailMap.firstKey();
+        }
+        return circle.get(hash);
+    }
+
+    /**
+     * Rebuild the cicle.
+     *
+     * @param hashAlgorithm The {@link HashAlgorithm Hash Algorithm}
+     * @param numberOfReplicas the number of replicas
+     * @param nodes nodes collection
+     */
+    public void rebuild(HashAlgorithm hashAlgorithm, Integer numberOfReplicas,
+            Collection<T> nodes) {
+        if (hashAlgorithm!=null) {
+            this.hashAlgorithm = hashAlgorithm;
+        }
+        if (numberOfReplicas!=null) {
+            this.numberOfReplicas = numberOfReplicas;
+        }
+
+        circle.clear();
+        for (T node : nodes) {
+            add(node);
+        }
+    }
+
+}
---
+++ router/src/main/java/io/galeb/router/consistenthash/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * <b>Provides simple consistent hash solution.</b>
+ * <p>
+ * Consistent hashing is based on mapping each object to a point on the
+ * edge of a circle (or equivalently, mapping each object to a real angle).
+ * <p>
+ * This package provides classes who maps each available node to many
+ * pseudo-randomly distributed points on the edge of the same circle.
+ */
+package io.galeb.router.consistenthash;

d1eb112e7a063cc48d2d2c893892100472b7ccd9
Marcelo Monteiro (tuxmonteiro) <marcelotmonteiro@gmail.com>
2017-04-13 15:00:16 -0300

    define dynamically healthcheck response

--- router/src/main/java/io/galeb/router/configurations/RootHandlerConfiguration.java
+++ router/src/main/java/io/galeb/router/configurations/RootHandlerConfiguration.java
@@ -3,6 +3,7 @@ package io.galeb.router.configurations;
 import io.galeb.router.completionListeners.AccessLogCompletionListener;
 import io.galeb.router.completionListeners.StatsdCompletionListener;
 import io.galeb.router.handlers.RootHandler;
+import io.galeb.router.services.ExternalData;
 import io.undertow.server.HttpHandler;
 import io.undertow.server.handlers.NameVirtualHostHandler;
 import io.undertow.util.Headers;
@@ -16,14 +17,17 @@ public class RootHandlerConfiguration {
     private final NameVirtualHostHandler nameVirtualHostHandler;
     private final AccessLogCompletionListener accessLogCompletionListener;
     private final StatsdCompletionListener statsdCompletionListener;
+    private final ExternalData data;
 
     @Autowired
     public RootHandlerConfiguration(final NameVirtualHostHandler nameVirtualHostHandler,
                                     final AccessLogCompletionListener accessLogCompletionListener,
-                                    final StatsdCompletionListener statsdCompletionListener) {
+                                    final StatsdCompletionListener statsdCompletionListener,
+                                    final ExternalData externalData) {
         this.nameVirtualHostHandler = nameVirtualHostHandler;
         this.accessLogCompletionListener = accessLogCompletionListener;
         this.statsdCompletionListener = statsdCompletionListener;
+        data = externalData;
         nameVirtualHostHandler.addHost("__ping__", pingHandler());
     }
 
@@ -31,7 +35,7 @@ public class RootHandlerConfiguration {
         return exchange -> {
             exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/plain");
             exchange.getResponseHeaders().put(Headers.SERVER, "GALEB");
-            exchange.getResponseSender().send("WORKING");
+            exchange.getResponseSender().send((data.exist(ExternalData.PREFIX_KEY) ? "WORKING" : "FAIL: " + ExternalData.PREFIX_KEY + " not found"));
         };
     }
 

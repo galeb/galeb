2809ba935a28a144ee5d9847b55bb9448a2ee94f
Marcelo Monteiro (tuxmonteiro) <marcelotmonteiro@gmail.com>
2017-04-15 03:31:50 -0300

    Externalized Undertow as Bean service

---
+++ router/src/main/java/io/galeb/router/configurations/UndertowConfiguration.java
@@ -0,0 +1,37 @@
+package io.galeb.router.configurations;
+
+import io.galeb.router.handlers.RootHandler;
+import io.undertow.Undertow;
+import io.undertow.UndertowOptions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.xnio.Options;
+
+@Configuration
+public class UndertowConfiguration {
+
+    private final int port = Integer.parseInt(SystemEnvs.ROUTER_PORT.getValue());
+    private final RootHandler rootHandler;
+
+    @Autowired
+    public UndertowConfiguration(final RootHandler rootHandler) {
+        this.rootHandler = rootHandler;
+    }
+
+    @Bean
+    public Undertow undertow() {
+        return Undertow.builder().addHttpListener(port, "0.0.0.0", rootHandler)
+                .setIoThreads(Integer.parseInt(SystemEnvs.IO_THREADS.getValue()))
+                .setWorkerThreads(Integer.parseInt(SystemEnvs.WORKER_THREADS.getValue()))
+                .setBufferSize(Integer.parseInt(SystemEnvs.BUFFER_SIZE.getValue()))
+                .setDirectBuffers(Boolean.parseBoolean(SystemEnvs.DIRECT_BUFFER.getValue()))
+                .setSocketOption(Options.BACKLOG, Integer.parseInt(SystemEnvs.BACKLOG.getValue()))
+                .setSocketOption(Options.KEEP_ALIVE, true)
+                .setSocketOption(Options.REUSE_ADDRESSES, true)
+                .setSocketOption(Options.TCP_NODELAY, true)
+                .setServerOption(UndertowOptions.RECORD_REQUEST_START_TIME, true)
+                .setServerOption(UndertowOptions.ENABLE_STATISTICS, true)
+                .build();
+    }
+}
--- router/src/main/java/io/galeb/router/services/RouterService.java
+++ router/src/main/java/io/galeb/router/services/RouterService.java
@@ -4,48 +4,28 @@
 
 package io.galeb.router.services;
 
-import io.galeb.router.configurations.SystemEnvs;
-import io.galeb.router.handlers.RootHandler;
 import io.undertow.Undertow;
-import io.undertow.UndertowOptions;
-import io.undertow.server.HttpHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.xnio.Options;
 
 import javax.annotation.PostConstruct;
 
 @Service
 public class RouterService {
 
-    private final int port = Integer.parseInt(SystemEnvs.ROUTER_PORT.getValue());
-
-    private final Logger      logger = LoggerFactory.getLogger(this.getClass());
-    private final HttpHandler rootHandler;
+    private final Logger   logger = LoggerFactory.getLogger(this.getClass());
+    private final Undertow undertow;
 
     @Autowired
-    public RouterService(final RootHandler rootHandler) {
-        this.rootHandler = rootHandler;
+    public RouterService(final Undertow undertow) {
+        this.undertow = undertow;
     }
 
     @PostConstruct
     public void run() {
         logger.info(this.getClass().getSimpleName() + " started");
-
-        final Undertow undertow = Undertow.builder().addHttpListener(port, "0.0.0.0", rootHandler)
-                .setIoThreads(Integer.parseInt(SystemEnvs.IO_THREADS.getValue()))
-                .setWorkerThreads(Integer.parseInt(SystemEnvs.WORKER_THREADS.getValue()))
-                .setBufferSize(Integer.parseInt(SystemEnvs.BUFFER_SIZE.getValue()))
-                .setDirectBuffers(Boolean.parseBoolean(SystemEnvs.DIRECT_BUFFER.getValue()))
-                .setSocketOption(Options.BACKLOG, Integer.parseInt(SystemEnvs.BACKLOG.getValue()))
-                .setSocketOption(Options.KEEP_ALIVE, true)
-                .setSocketOption(Options.REUSE_ADDRESSES, true)
-                .setSocketOption(Options.TCP_NODELAY, true)
-                .setServerOption(UndertowOptions.RECORD_REQUEST_START_TIME, true)
-                .setServerOption(UndertowOptions.ENABLE_STATISTICS, true)
-                .build();
         undertow.start();
     }
 }

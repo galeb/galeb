9140ff5d1a105273c42f8c83d0f0db1dfd1714fd
Marcelo Monteiro (tuxmonteiro) <marcelotmonteiro@gmail.com>
2017-04-20 14:39:48 -0300

    Copyright headers

--- health/src/main/java/io/galeb/health/SystemEnvs.java
+++ health/src/main/java/io/galeb/health/SystemEnvs.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2014-2017 Globo.com - ATeam
+ * All rights reserved.
+ *
+ * This source is subject to the Apache License, Version 2.0.
+ * Please see the LICENSE file for more information.
+ *
+ * Authors: See AUTHORS file
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.galeb.health;
 
 import java.io.IOException;

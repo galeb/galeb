af3f87645f50663a37a12e81367f6ffedabc156c
Marcelo Monteiro (tuxmonteiro) <marcelotmonteiro@gmail.com>
2017-04-21 02:32:30 -0300

    RootHandler: Using AbstractUpdater

--- router/src/main/java/io/galeb/router/handlers/RootHandler.java
+++ router/src/main/java/io/galeb/router/handlers/RootHandler.java
@@ -1,21 +1,17 @@
 package io.galeb.router.handlers;
 
-import io.galeb.router.client.ExtendedProxyClient;
-import io.galeb.router.handlers.completionListeners.AccessLogCompletionListener;
-import io.galeb.router.handlers.completionListeners.StatsdCompletionListener;
 import io.galeb.router.ResponseCodeOnError;
 import io.galeb.router.SystemEnvs;
+import io.galeb.router.handlers.completionListeners.AccessLogCompletionListener;
+import io.galeb.router.handlers.completionListeners.StatsdCompletionListener;
 import io.undertow.server.HttpHandler;
 import io.undertow.server.HttpServerExchange;
-import io.undertow.server.handlers.IPAddressAccessControlHandler;
 import io.undertow.server.handlers.NameVirtualHostHandler;
-import io.undertow.server.handlers.proxy.ProxyHandler;
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
-import java.util.Map;
-
-public class RootHandler implements HttpHandler {
+public class RootHandler extends AbstractUpdater implements HttpHandler {
 
     private final Log logger = LogFactory.getLog(this.getClass());
 
@@ -29,7 +25,7 @@ public class RootHandler implements HttpHandler {
     public RootHandler(final NameVirtualHostHandler nameVirtualHostHandler,
                        final AccessLogCompletionListener accessLogCompletionListener,
                        final StatsdCompletionListener statsdCompletionListener) {
-
+        super(nameVirtualHostHandler);
         this.nameVirtualHostHandler = nameVirtualHostHandler;
         this.accessLogCompletionListener = accessLogCompletionListener;
         this.statsdCompletionListener = statsdCompletionListener;
@@ -43,66 +39,9 @@ public class RootHandler implements HttpHandler {
 
             nameVirtualHostHandler.handleRequest(exchange);
         } catch (Exception e) {
+            logger.error(ExceptionUtils.getStackTrace(e));
             ResponseCodeOnError.ROOT_HANDLER_FAILED.getHandler().handleRequest(exchange);
         }
     }
 
-    public synchronized void forceAllUpdate() {
-        nameVirtualHostHandler.getHosts().forEach((virtualhost, handler) -> forceVirtualhostUpdate(virtualhost));
-    }
-
-    public synchronized void forceVirtualhostUpdate(String virtualhost) {
-        if ("__ping__".equals(virtualhost)) return;
-        if (nameVirtualHostHandler.getHosts().containsKey(virtualhost)) {
-            logger.warn("[" + virtualhost + "] FORCING UPDATE");
-            final HttpHandler handler = nameVirtualHostHandler.getHosts().get(virtualhost);
-            if (handler instanceof RuleTargetHandler) {
-                HttpHandler ruleTargetNextHandler = ((RuleTargetHandler) handler).getNext();
-                if (ruleTargetNextHandler instanceof IPAddressAccessControlHandler) {
-                    ruleTargetNextHandler = ((IPAddressAccessControlHandler)ruleTargetNextHandler).getNext();
-                }
-                if (ruleTargetNextHandler instanceof PathGlobHandler) {
-                    final PathGlobHandler pathGlobHandler = (PathGlobHandler) ruleTargetNextHandler;
-                    pathGlobHandler.getPaths().forEach((k, poolHandler) -> {
-                        final ProxyHandler proxyHandler = ((PoolHandler) poolHandler).getProxyHandler();
-                        if (proxyHandler != null) {
-                            final ExtendedProxyClient proxyClient = (ExtendedProxyClient) proxyHandler.getProxyClient();
-                            proxyClient.removeAllHosts();
-                        }
-                    });
-                    pathGlobHandler.clear();
-                }
-            }
-            nameVirtualHostHandler.removeHost(virtualhost);
-        }
-    }
-
-    public synchronized void forcePoolUpdate(String poolName) {
-        nameVirtualHostHandler.getHosts().entrySet().stream()
-                .filter(e -> e.getValue() instanceof RuleTargetHandler).forEach(entryHost ->
-        {
-            final String virtualhost = entryHost.getKey();
-            final HttpHandler handler = ((RuleTargetHandler)entryHost.getValue()).getNext();
-            if (handler != null) {
-                if (handler instanceof PathGlobHandler) {
-                    forcePoolUpdateByPathGlobHandler(poolName, virtualhost, (PathGlobHandler) handler);
-                }
-                if (handler instanceof IPAddressAccessControlHandler) {
-                    forcePoolUpdateByIpAclHandler(poolName, virtualhost, (IPAddressAccessControlHandler) handler);
-                }
-            }
-        });
-    }
-
-    private synchronized void forcePoolUpdateByPathGlobHandler(String poolName, String virtualhost, PathGlobHandler handler) {
-        handler.getPaths().entrySet().stream().map(Map.Entry::getValue)
-                .filter(pathHandler -> pathHandler instanceof PoolHandler &&
-                        ((PoolHandler) pathHandler).getPoolname() != null &&
-                        ((PoolHandler) pathHandler).getPoolname().equals(poolName))
-                .forEach(v -> forceVirtualhostUpdate(virtualhost));
-    }
-
-    private synchronized void forcePoolUpdateByIpAclHandler(String poolName, String virtualhost, IPAddressAccessControlHandler handler) {
-        forcePoolUpdateByPathGlobHandler(poolName, virtualhost, (PathGlobHandler) handler.getNext());
-    }
 }

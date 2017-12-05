959e3f1b3d1311da33b948519d208ec4f195f4d8
Marcelo Monteiro (tuxmonteiro) <marcelotmonteiro@gmail.com>
2017-12-05 17:45:20 -0200

    Add kraken, legba and oldapi services

---
+++ oldapi/src/test/java/io/galeb/oldapi/ApplicationTest.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2017-2017 Globo.com
+ * All rights reserved.
+ *
+ * This source is subject to the Apache License, Version 2.0.
+ * Please see the LICENSE file for more information.
+ *
+ * Authors: See AUTHORS file
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.galeb.oldapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SuppressWarnings("EmptyMethod")
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApplicationTest {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}

34b5b9d2eb4c73efe2217bcf8e1f59a149a7ca64
Marcelo Monteiro (tuxmonteiro) <marcelotmonteiro@gmail.com>
2017-12-08 18:23:25 -0200

    Kratos: improve datasource configuration

--- kratos/src/main/java/io/galeb/kratos/configurations/DatabaseConfiguration.java
+++ kratos/src/main/java/io/galeb/kratos/configurations/DatabaseConfiguration.java
@@ -1,7 +1,11 @@
 package io.galeb.kratos.configurations;
 
+import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -10,4 +14,15 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 @EnableJpaRepositories(basePackages = "io.galeb.kratos.repository")
 public class DatabaseConfiguration {
+
+    @Bean
+    @Primary
+    public DataSourceProperties dataSourceProperties() {
+        return new DataSourceProperties();
+    }
+
+    @Bean
+    public HikariDataSource dataSource(DataSourceProperties properties) {
+        return (HikariDataSource) properties.initializeDataSourceBuilder().type(HikariDataSource.class).build();
+    }
 }

785f7ee5987c97d0e8ebd6a8bd284e2dba307658
Marcelo Monteiro (tuxmonteiro) <marcelotmonteiro@gmail.com>
2017-12-08 19:36:33 -0200

    Improve controllers

--- kratos/src/main/java/io/galeb/kratos/controllers/HealthcheckController.java
+++ kratos/src/main/java/io/galeb/kratos/controllers/HealthcheckController.java
@@ -1,12 +1,14 @@
 package io.galeb.kratos.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthcheckController {
 
-    @GetMapping("/healthcheck")
+    @GetMapping(value = "/healthcheck")
+    @ResponseBody
     public String healthcheck() {
         return "WORKING";
     }

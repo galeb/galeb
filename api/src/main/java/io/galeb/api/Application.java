ff1cd8a3df184bc6308d60d36e02f168c8d3137e
Marcelo Monteiro (tuxmonteiro) <marcelotmonteiro@gmail.com>
2017-12-12 14:27:09 -0200

    Improve API service, using properties and applied minor cleanup/fix

--- api/src/main/java/io/galeb/api/Application.java
+++ api/src/main/java/io/galeb/api/Application.java
@@ -17,10 +17,8 @@
 package io.galeb.api;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EntityScan( basePackages = {"io.galeb.core.entity"} )
---
+++ api/src/main/java/io/galeb/api/controllers/InfoController.java
@@ -0,0 +1,32 @@
+package io.galeb.api.controllers;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SuppressWarnings("unused")
+@RestController
+public class InfoController {
+
+    @Value("${build.project}")
+    private String buildProject;
+
+    @Value("${build.version}")
+    private String buildVersion;
+
+    @Value("${build.timestamp}")
+    private String buildTimestamp;
+
+    @GetMapping(value = "/info")
+    public ResponseEntity<String> info() {
+        String body = String.format("{\"name\":\"%s\", \"version\":\"%s\", \"build\":\"%s\"}", buildProject, buildVersion, buildTimestamp);
+        MultiValueMap<String, String> headers = new LinkedMultiValueMap<>();
+        headers.add("content-type", MediaType.APPLICATION_JSON_VALUE);
+        return new ResponseEntity<>(body, headers, HttpStatus.OK);
+    }
+}

0126f6494d4a7e6ef225726d3527091b23c39099
Marcelo Monteiro (tuxmonteiro) <marcelotmonteiro@gmail.com>
2017-12-18 19:04:37 -0200

    RepositoryEventListener: Applied best approach

--- api/src/main/java/io/galeb/api/handler/TargetHandler.java
+++ api/src/main/java/io/galeb/api/handler/TargetHandler.java
@@ -1,14 +1,9 @@
 package io.galeb.api.handler;
 
-import io.galeb.core.entity.AbstractEntity;
 import io.galeb.core.entity.Target;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TargetHandler extends AbstractHandler<Target> {
 
-    @Override
-    public Class<? extends AbstractEntity> entityClass() {
-        return Target.class;
-    }
 }

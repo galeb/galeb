41b27bdaa59f63e3c380ffb56b312900d1d7da34
Marcelo Monteiro (tuxmonteiro) <marcelotmonteiro@gmail.com>
2017-12-18 18:58:02 -0200

    Improve handlers

--- api/src/main/java/io/galeb/api/handler/TargetHandler.java
+++ api/src/main/java/io/galeb/api/handler/TargetHandler.java
@@ -1,8 +1,14 @@
 package io.galeb.api.handler;
 
+import io.galeb.core.entity.AbstractEntity;
 import io.galeb.core.entity.Target;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TargetHandler extends AbstractHandler<Target> {
+
+    @Override
+    public Class<? extends AbstractEntity> entityClass() {
+        return Target.class;
+    }
 }
--- api/src/main/java/io/galeb/api/handler/VirtualHostHandler.java
+++ api/src/main/java/io/galeb/api/handler/VirtualHostHandler.java
@@ -1,6 +1,7 @@
 package io.galeb.api.handler;
 
 import io.galeb.api.repository.RuleGroupRepository;
+import io.galeb.core.entity.AbstractEntity;
 import io.galeb.core.entity.RuleGroup;
 import io.galeb.core.entity.VirtualHost;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +13,7 @@ public class VirtualHostHandler extends AbstractHandler<VirtualHost> {
     @Autowired
     RuleGroupRepository ruleGroupRepository;
 
+    @Override
     public void beforeCreate(VirtualHost virtualHost) {
         if (virtualHost.getRulegroup() == null) {
             RuleGroup ruleGroup = new RuleGroup();
@@ -20,7 +22,8 @@ public class VirtualHostHandler extends AbstractHandler<VirtualHost> {
         }
     }
 
-    public void beforeDelete(VirtualHost virtualHost) {
-
+    @Override
+    public Class<? extends AbstractEntity> entityClass() {
+        return VirtualHost.class;
     }
 }

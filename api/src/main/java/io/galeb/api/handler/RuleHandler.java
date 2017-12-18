84ebccf34d278983563873b77463f8b822dcd15a
Marcelo Monteiro (tuxmonteiro) <marcelotmonteiro@gmail.com>
2017-12-18 17:34:57 -0200

    Improve Handler/RepositoryEventListener and auto create RuleGroup if necessary

--- api/src/main/java/io/galeb/api/handler/RuleHandler.java
+++ api/src/main/java/io/galeb/api/handler/RuleHandler.java
@@ -1,8 +1,8 @@
 package io.galeb.api.handler;
 
 import io.galeb.core.entity.Rule;
-import org.springframework.data.rest.core.annotation.RepositoryEventHandler;
+import org.springframework.stereotype.Component;
 
-@RepositoryEventHandler
+@Component
 public class RuleHandler extends AbstractHandler<Rule> {
 }
--- api/src/main/java/io/galeb/api/handler/VirtualHostHandler.java
+++ api/src/main/java/io/galeb/api/handler/VirtualHostHandler.java
@@ -1,8 +1,26 @@
 package io.galeb.api.handler;
 
+import io.galeb.api.repository.RuleGroupRepository;
+import io.galeb.core.entity.RuleGroup;
 import io.galeb.core.entity.VirtualHost;
-import org.springframework.data.rest.core.annotation.RepositoryEventHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
-@RepositoryEventHandler
+@Component
 public class VirtualHostHandler extends AbstractHandler<VirtualHost> {
+
+    @Autowired
+    RuleGroupRepository ruleGroupRepository;
+
+    public void beforeCreate(VirtualHost virtualHost) {
+        if (virtualHost.getRulegroup() == null) {
+            RuleGroup ruleGroup = new RuleGroup();
+            ruleGroupRepository.save(ruleGroup);
+            virtualHost.setRulegroup(ruleGroup);
+        }
+    }
+
+    public void beforeDelete(VirtualHost virtualHost) {
+
+    }
 }

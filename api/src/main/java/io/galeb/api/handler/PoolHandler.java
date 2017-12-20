7f8c6a28cd1a8c2190f5292f434174e629dc21a4
Tiago Machado <tiago.machado@corp.globo.com>
2017-12-20 13:11:20 -0200

    Added registering changes in redis

--- api/src/main/java/io/galeb/api/handler/PoolHandler.java
+++ api/src/main/java/io/galeb/api/handler/PoolHandler.java
@@ -1,9 +1,18 @@
 package io.galeb.api.handler;
 
+import com.google.common.collect.Sets;
+import io.galeb.core.entity.Environment;
 import io.galeb.core.entity.Pool;
 import org.springframework.stereotype.Component;
 
+import java.util.Set;
+
 @Component
 public class PoolHandler extends AbstractHandler<Pool> {
+
+    @Override
+    protected Set<Environment> getAllEnvironments(Pool entity) {
+        return Sets.newHashSet(entity.getEnvironment());
+    }
 
 }
--- api/src/main/java/io/galeb/api/handler/TargetHandler.java
+++ api/src/main/java/io/galeb/api/handler/TargetHandler.java
@@ -1,11 +1,19 @@
 package io.galeb.api.handler;
 
+import io.galeb.api.repository.EnvironmentRepository;
 import io.galeb.core.exceptions.BadRequestException;
+import io.galeb.core.entity.Environment;
 import io.galeb.core.entity.Target;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.Set;
 
 @Component
 public class TargetHandler extends AbstractHandler<Target> {
+
+    @Autowired
+    EnvironmentRepository environmentRepository;
 
     @Override
     protected void onBeforeCreate(Target entity) {
@@ -14,4 +22,10 @@ public class TargetHandler extends AbstractHandler<Target> {
             throw new BadRequestException("Pool(s) undefined");
         }
     }
+
+    @Override
+    protected Set<Environment> getAllEnvironments(Target entity) {
+        return environmentRepository.findAllByTargetId(entity.getId());
+    }
+
 }

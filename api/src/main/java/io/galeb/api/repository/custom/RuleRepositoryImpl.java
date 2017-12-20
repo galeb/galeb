cdcd2b28c51711cde8efaed92ae1eceb2f913066
Marcelo Monteiro (tuxmonteiro) <marcelotmonteiro@gmail.com>
2017-12-20 01:54:37 -0200

    Remove unnecessaries overrides

--- api/src/main/java/io/galeb/api/repository/custom/RuleRepositoryImpl.java
+++ api/src/main/java/io/galeb/api/repository/custom/RuleRepositoryImpl.java
@@ -3,9 +3,6 @@ package io.galeb.api.repository.custom;
 import io.galeb.api.services.StatusService;
 import io.galeb.core.entity.Rule;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 
 import javax.annotation.PostConstruct;
 import javax.persistence.EntityManager;
@@ -26,18 +23,4 @@ public class RuleRepositoryImpl extends AbstractRepositoryImplementation<Rule> i
         setStatusService(statusService);
     }
 
-    @Override
-    public Rule findOne(Long id) {
-        return super.findOne(id);
-    }
-
-    @Override
-    public Iterable<Rule> findAll(Sort sort) {
-        return super.findAll(sort);
-    }
-
-    @Override
-    public Page<Rule> findAll(Pageable pageable) {
-        return super.findAll(pageable);
-    }
 }

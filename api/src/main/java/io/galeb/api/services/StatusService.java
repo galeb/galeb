a3153682850c519bdf6467fed6a3dbdb1c753cf5
Marcelo Monteiro (tuxmonteiro) <marcelotmonteiro@gmail.com>
2017-12-20 02:50:44 -0200

    StatusService: minor cleanup

--- api/src/main/java/io/galeb/api/services/StatusService.java
+++ api/src/main/java/io/galeb/api/services/StatusService.java
@@ -1,14 +1,13 @@
 package io.galeb.api.services;
 
 import io.galeb.core.entity.AbstractEntity;
-import io.galeb.core.entity.WithStatus;
 import io.galeb.core.entity.WithStatus.Status;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StatusService {
 
-    public Status status(WithStatus entity, Long id) {
-        return ((AbstractEntity)entity).isQuarantine() ? Status.DELETED : Status.OK;
+    public Status status(AbstractEntity entity) {
+        return entity.isQuarantine() ? Status.DELETED : Status.OK;
     }
 }

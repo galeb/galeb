ae587248bc0d4544e1662b48b597a61222ba82af
Marcelo Monteiro (tuxmonteiro) <marcelotmonteiro@gmail.com>
2017-12-20 01:54:37 -0200

    Remove unnecessaries overrides

--- api/src/main/java/io/galeb/api/repository/custom/EnvironmentRepositoryImpl.java
+++ api/src/main/java/io/galeb/api/repository/custom/EnvironmentRepositoryImpl.java
@@ -3,9 +3,6 @@ package io.galeb.api.repository.custom;
 import io.galeb.api.services.StatusService;
 import io.galeb.core.entity.Environment;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 
 import javax.annotation.PostConstruct;
 import javax.persistence.EntityManager;
@@ -26,18 +23,4 @@ public class EnvironmentRepositoryImpl extends AbstractRepositoryImplementation<
         setStatusService(statusService);
     }
 
-    @Override
-    public Environment findOne(Long id) {
-        return super.findOne(id);
-    }
-
-    @Override
-    public Iterable<Environment> findAll(Sort sort) {
-        return super.findAll(sort);
-    }
-
-    @Override
-    public Page<Environment> findAll(Pageable pageable) {
-        return super.findAll(pageable);
-    }
 }
--- api/src/main/java/io/galeb/api/repository/custom/VirtualHostRepositoryImpl.java
+++ api/src/main/java/io/galeb/api/repository/custom/VirtualHostRepositoryImpl.java
@@ -3,9 +3,6 @@ package io.galeb.api.repository.custom;
 import io.galeb.api.services.StatusService;
 import io.galeb.core.entity.VirtualHost;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 
 import javax.annotation.PostConstruct;
 import javax.persistence.EntityManager;
@@ -24,20 +21,5 @@ public class VirtualHostRepositoryImpl extends AbstractRepositoryImplementation<
     private void init() {
         setSimpleJpaRepository(VirtualHost.class, em);
         setStatusService(statusService);
-    }
-
-    @Override
-    public VirtualHost findOne(Long id) {
-        return super.findOne(id);
-    }
-
-    @Override
-    public Iterable<VirtualHost> findAll(Sort sort) {
-        return super.findAll(sort);
-    }
-
-    @Override
-    public Page<VirtualHost> findAll(Pageable pageable) {
-        return super.findAll(pageable);
     }
 }

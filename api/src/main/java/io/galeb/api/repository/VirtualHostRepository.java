09ad4a71850b1219a1877f0bd08ce3d2f9df8805
Marcelo Monteiro (tuxmonteiro) <marcelotmonteiro@gmail.com>
2017-12-20 11:57:37 -0200

    Remove unnecessary "extends"

--- api/src/main/java/io/galeb/api/repository/VirtualHostRepository.java
+++ api/src/main/java/io/galeb/api/repository/VirtualHostRepository.java
@@ -1,11 +1,10 @@
 package io.galeb.api.repository;
 
-import io.galeb.api.repository.custom.VirtualHostRepositoryCustom;
 import io.galeb.core.entity.VirtualHost;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(path = "virtualhost", collectionResourceRel = "virtualhost")
-public interface VirtualHostRepository extends JpaRepository<VirtualHost, Long>, VirtualHostRepositoryCustom {
+public interface VirtualHostRepository extends JpaRepository<VirtualHost, Long> {
 
 }

843210bb85aba73be3a56a691b95d889114388a8
Tiago Machado <tiago.machado@corp.globo.com>
2017-12-21 10:30:44 -0200

    Improving formats keys redis

--- api/src/main/java/io/galeb/api/services/StatusService.java
+++ api/src/main/java/io/galeb/api/services/StatusService.java
@@ -2,12 +2,23 @@ package io.galeb.api.services;
 
 import io.galeb.core.entity.AbstractEntity;
 import io.galeb.core.entity.WithStatus.Status;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 @Service
 public class StatusService {
 
+    @Autowired
+    ChangesService changesService;
+
     public Status status(AbstractEntity entity) {
-        return entity.isQuarantine() ? Status.DELETED : Status.OK;
+        Status status = Status.OK;
+        if (changesService.has(entity)) {
+            status = entity.isQuarantine() ? Status.DELETED : Status.PENDING;
+        }
+        return status;
     }
+
 }

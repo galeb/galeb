27540697466bfe7d39224e8274003060706faad5
Marcelo Monteiro (tuxmonteiro) <marcelotmonteiro@gmail.com>
2017-12-21 20:14:03 -0200

    Spring Security: initial implementation

--- api/src/main/java/io/galeb/api/repository/EnvironmentRepository.java
+++ api/src/main/java/io/galeb/api/repository/EnvironmentRepository.java
@@ -6,11 +6,20 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 import java.util.Set;
 
 @RepositoryRestResource(path = "environment", collectionResourceRel = "environment", itemResourceRel = "environment")
 public interface EnvironmentRepository extends JpaRepository<Environment, Long> {
+
+    @Override
+    @PreAuthorize("hasRole('ROLE_ADMIN') or principal.username == 'admin'")
+    Environment save(Environment environment);
+
+    @Override
+    @PreAuthorize("hasRole('ROLE_ADMIN') or principal.username == 'admin'")
+    void delete(Long id);
 
     @RestResource(exported = false)
     @Query(value = "SELECT DISTINCT e FROM Environment as e " +

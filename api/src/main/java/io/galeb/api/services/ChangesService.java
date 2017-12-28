beebf16b5145b1ce789237a2d37c7fc8b52c516f
Tiago Machado <tiago.machado@corp.globo.com>
2017-12-28 16:35:18 -0200

    Some refactories for organization

--- api/src/main/java/io/galeb/api/services/ChangesService.java
+++ api/src/main/java/io/galeb/api/services/ChangesService.java
@@ -3,13 +3,11 @@ package io.galeb.api.services;
 import io.galeb.core.entity.AbstractEntity;
 import io.galeb.core.entity.Environment;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.text.MessageFormat;
 import java.text.ParsePosition;
-import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -25,30 +23,16 @@ public class ChangesService {
      */
     private static final String FORMAT_KEY_HAS_CHANGE = "haschange:{0}:{1}:{2}:{3}";
 
-    /**
-     * Description arguments:
-     * {0} - Environment Id
-     */
-    private static final String FORMAT_KEY_VERSION = "version:{0}";
-
     @Autowired
     StringRedisTemplate redisTemplate;
 
-    public void register(Environment e, AbstractEntity entity) {
-        long envId = e.getId();
-        Long newVersion = incrementVersion(envId);
+    public void register(Environment e, AbstractEntity entity, String version) {
         String simpleNameClass = entity.getClass().getSimpleName().toLowerCase();
+        String envId = String.valueOf(e.getId());
         Long entityId = entity.getId();
         String entityLastModifiedAt = String.valueOf(entity.getLastModifiedAt().getTime());
         String keyFormatted = MessageFormat.format(FORMAT_KEY_HAS_CHANGE, envId, simpleNameClass, entityId, entityLastModifiedAt);
-        redisTemplate.opsForValue().setIfAbsent(keyFormatted, String.valueOf(newVersion));
-    }
-
-    public boolean has(AbstractEntity entity) {
-        String simpleNameClass = entity.getClass().getSimpleName().toLowerCase();
-        Long entityId = entity.getId();
-        String keyFormatted = MessageFormat.format(FORMAT_KEY_HAS_CHANGE, "*",simpleNameClass, entityId, "*");
-        return hasKey(keyFormatted);
+        redisTemplate.opsForValue().setIfAbsent(keyFormatted, version);
     }
 
     public boolean hasByEnvironmentId(Long environmentId) {
@@ -64,11 +48,6 @@ public class ChangesService {
         Set<String> keys = keys(keyFormatted);
         Set<Long> environmentIds = keys.stream().map(k -> Long.parseLong(messageFormat.parse(k, new ParsePosition(0))[0].toString())).collect(Collectors.toSet());
         return environmentIds;
-    }
-
-    private Long incrementVersion(long envId) {
-        String keyFormatted = MessageFormat.format(FORMAT_KEY_VERSION, envId);
-        return redisTemplate.opsForValue().increment(keyFormatted, 1);
     }
 
     private boolean hasKey(String key) {

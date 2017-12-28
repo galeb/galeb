182506d528f03b18e2513c5d9774d2928956af31
Tiago Machado <tiago.machado@corp.globo.com>
2017-12-28 17:44:06 -0200

    Added repository virtualhost

--- legba/src/main/java/io/galeb/legba/services/CopyService.java
+++ legba/src/main/java/io/galeb/legba/services/CopyService.java
@@ -1,16 +1,21 @@
 package io.galeb.legba.services;
 
 import io.galeb.core.entity.VirtualHost;
+import io.galeb.legba.repository.VirtualHostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Collections;
 import java.util.List;
 
 @Service
 public class CopyService {
 
+    @Autowired
+    private VirtualHostRepository virtualHostRepository;
+
     public List<VirtualHost> getVirtualHosts(String envid, String numRouters, String version) {
-        return Collections.emptyList();
+        List<VirtualHost> list = virtualHostRepository.findAll();
+        return list;
     }
 
 }

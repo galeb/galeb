9af3aaa8e6c2108350a254b36423c9736f758dd3
Marcelo Monteiro (tuxmonteiro) <marcelotmonteiro@gmail.com>
2018-01-02 11:42:12 -0200

    Move DatabaseConfiguration to core module

--- newcore/src/main/java/io/galeb/core/configuration/DatabaseConfiguration.java
+++ newcore/src/main/java/io/galeb/core/configuration/DatabaseConfiguration.java
@@ -1,18 +1,14 @@
-package io.galeb.api.configuration;
+package io.galeb.core.configuration;
 
 import com.zaxxer.hikari.HikariDataSource;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
-@EntityScan("io.galeb.core.entity")
 @EnableTransactionManagement
-@EnableJpaRepositories(basePackages = "io.galeb.api.repository")
 public class DatabaseConfiguration {
 
     @Bean

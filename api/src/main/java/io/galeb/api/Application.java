804cbdd67c48dbfc1e1b5f4a4119d1ff8721abd0
Marcelo Monteiro (tuxmonteiro) <marcelotmonteiro@gmail.com>
2018-01-02 11:42:12 -0200

    Move DatabaseConfiguration to core module

--- api/src/main/java/io/galeb/api/Application.java
+++ api/src/main/java/io/galeb/api/Application.java
@@ -20,10 +20,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EntityScan( basePackages = {"io.galeb.core.entity"} )
 @ComponentScan(basePackages = {"io.galeb.core.services", "io.galeb.core.configuration", "io.galeb.api"})
+@EnableJpaRepositories(basePackages = "io.galeb.api.repository")
 public class Application {
 
     public static void main(String args[]) {

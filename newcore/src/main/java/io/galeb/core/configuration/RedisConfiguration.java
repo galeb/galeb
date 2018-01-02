c0b3448b8532fb8e4dc2c83dbc5f633e28c14a00
Marcelo Monteiro (tuxmonteiro) <marcelotmonteiro@gmail.com>
2018-01-02 16:02:40 -0200

    Using spring.redis.url instead sprinf.redis.{host, port, sentinel, etc}

--- newcore/src/main/java/io/galeb/core/configuration/RedisConfiguration.java
+++ newcore/src/main/java/io/galeb/core/configuration/RedisConfiguration.java
@@ -1,21 +1,26 @@
 package io.galeb.core.configuration;
 
+import com.lambdaworks.redis.RedisURI;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisSentinelConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
-
-import java.util.Arrays;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import static com.lambdaworks.redis.RedisURI.URI_SCHEME_REDIS_SENTINEL;
+
 @Configuration
 public class RedisConfiguration {
+
+    private static final Logger LOGGER = LogManager.getLogger(RedisConfiguration.class);
 
     @Bean
     @Primary
@@ -25,14 +30,20 @@ public class RedisConfiguration {
 
     @Bean
     public RedisConnectionFactory lettuceConnectionFactory(RedisProperties redisProperties) {
-        if (redisProperties.getSentinel() != null) {
-            Set nodesSentinel = Arrays.stream(redisProperties.getSentinel().getNodes().split(",")).collect(Collectors.toSet());
-            RedisSentinelConfiguration redisSentinelConfiguration = new RedisSentinelConfiguration(redisProperties.getSentinel().getMaster(), nodesSentinel);
-            return new LettuceConnectionFactory(redisSentinelConfiguration);
+        RedisURI redisURI = RedisURI.create(redisProperties().getUrl());
+        final LettuceConnectionFactory connectionFactory;
+        if (redisProperties.getUrl().startsWith(URI_SCHEME_REDIS_SENTINEL)) {
+            final Set<String> nodesSentinel = redisURI.getSentinels().stream().map(r -> r.getHost() + ":" + r.getPort()).collect(Collectors.toSet());
+            RedisSentinelConfiguration redisSentinelConfiguration = new RedisSentinelConfiguration(redisURI.getSentinelMasterId(), nodesSentinel);
+            connectionFactory = new LettuceConnectionFactory(redisSentinelConfiguration);
+            LOGGER.info("Using Redis Sentinel");
         } else {
-            return new LettuceConnectionFactory(redisProperties.getHost(), redisProperties.getPort());
+            connectionFactory = new LettuceConnectionFactory(redisURI.getHost(), redisURI.getPort());
+            LOGGER.info("Using Redis Standalone");
         }
-
+        if (redisURI.getPassword() != null) connectionFactory.setPassword(new String(redisURI.getPassword()));
+        if (redisURI.getDatabase() != 0) connectionFactory.setDatabase(redisURI.getDatabase());
+        return connectionFactory;
     }
 
     @Bean(name = "redisTemplate")

240c0da71741c33199dddc6182c7fb4bde5945a5
thaissa.falbo <thaissa.falbo@gmail.com>
2018-01-02 14:47:41 -0200

    Removes all database on reset

--- api/src/test/java/io/galeb/api/cucumber/test/StepDefs.java
+++ api/src/test/java/io/galeb/api/cucumber/test/StepDefs.java
@@ -71,7 +71,6 @@ public class StepDefs {
 
     private RequestSpecification request;
     private ValidatableResponse response;
-    private String token;
 
     private RedirectConfig redirectConfig = RestAssuredConfig.config().getRedirectConfig().followRedirects(false);
     private RestAssuredConfig restAssuredConfig = RestAssuredConfig.config().redirect(redirectConfig);

c138c09ce6d7113ad6c36bde8d3f8f0e8174b908
Marcelo Monteiro (tuxmonteiro) <marcelotmonteiro@gmail.com>
2018-01-05 17:08:54 -0200

    Fix wrong imports

--- api/src/main/java/io/galeb/api/repository/BalancePolicyRepository.java
+++ api/src/main/java/io/galeb/api/repository/BalancePolicyRepository.java
@@ -2,12 +2,11 @@ package io.galeb.api.repository;
 
 import io.galeb.core.entity.BalancePolicy;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.security.access.prepost.PreAuthorize;
-
-import java.awt.print.Pageable;
 
 @RepositoryRestResource(path = "balancepolicy", collectionResourceRel = "balancepolicy", itemResourceRel = "balancepolicy")
 public interface BalancePolicyRepository extends JpaRepository<BalancePolicy, Long> {

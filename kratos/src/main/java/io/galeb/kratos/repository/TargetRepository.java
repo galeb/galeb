9a3559c510ad124585eed72fb37cac0f819f7592
Igor Fernandes <igor.fernandes@corp.globo.com>
2018-01-08 16:21:49 -0200

    Kratos: fix findByEnvironmentName query

--- kratos/src/main/java/io/galeb/kratos/repository/TargetRepository.java
+++ kratos/src/main/java/io/galeb/kratos/repository/TargetRepository.java
@@ -5,12 +5,13 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(exported = false)
 public interface TargetRepository extends JpaRepository<Target, Long> {
 
-    @Query("SELECT t FROM Target t INNER JOIN t.pools p WHERE p.environment = ?1")
-    Page<Target> findByEnvironmentName(String env, Pageable pageable);
+    @Query("SELECT t FROM Target t INNER JOIN t.pools p WHERE p.environment.name = :env")
+    Page<Target> findByEnvironmentName(@Param("env") String env, Pageable pageable);
 
 }

7291935e3f8170dfdfb931278afb457204185e67
Marcelo Monteiro (tuxmonteiro) <marcelotmonteiro@gmail.com>
2018-01-10 09:03:49 -0200

    Fix AccountRepository.roles and TeamRepository.roles

--- api/src/main/java/io/galeb/api/repository/custom/AccountRepositoryImpl.java
+++ api/src/main/java/io/galeb/api/repository/custom/AccountRepositoryImpl.java
@@ -2,11 +2,13 @@ package io.galeb.api.repository.custom;
 
 import io.galeb.api.services.StatusService;
 import io.galeb.core.entity.Account;
+import io.galeb.core.entity.RoleGroup;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.annotation.PostConstruct;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -26,8 +28,15 @@ public class AccountRepositoryImpl extends AbstractRepositoryImplementation<Acco
 
     @Override
     public Set<String> roles(Object principal, Object criteria) {
-        return ((Account) principal).getRolegroups().stream().flatMap(rg -> rg.getRoles().stream())
-                .map(Object::toString).collect(Collectors.toSet());
+        Account account = ((Account) principal);
+        Set<String> roles = account.getRolegroups().stream().flatMap(rg -> rg.getRoles().stream())
+                .map(Object::toString).distinct().collect(Collectors.toSet());
+        List<RoleGroup> roleGroups = em.createNamedQuery("roleGroupsFromTeams", RoleGroup.class)
+                .setParameter("id", account.getId())
+                .getResultList();
+        roles.addAll(roleGroups.stream().flatMap(rg -> rg.getRoles().stream())
+                .map(Object::toString).distinct().collect(Collectors.toSet()));
+        return roles;
     }
 
 }
--- api/src/main/java/io/galeb/api/repository/custom/TeamRepositoryImpl.java
+++ api/src/main/java/io/galeb/api/repository/custom/TeamRepositoryImpl.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import javax.annotation.PostConstruct;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -30,11 +31,30 @@ public class TeamRepositoryImpl extends AbstractRepositoryImplementation<Team> i
     @Override
     public Set<String> roles(Object principal, Object criteria) {
         Account account = (Account) principal;
-        List<RoleGroup> roleGroups = em.createNamedQuery("roleGroupsTeam", RoleGroup.class)
-                .setParameter("team_id", ((Team)criteria).getId())
-                .setParameter("account_id", account.getId())
-                .getResultList();
-        return roleGroups.stream().flatMap(rg -> rg.getRoles().stream()).map(Enum::toString).collect(Collectors.toSet());
+        List<RoleGroup> roleGroups;
+        if (criteria instanceof Team) {
+            roleGroups = em.createNamedQuery("roleGroupsTeam", RoleGroup.class)
+                    .setParameter("team_id", ((Team)criteria).getId())
+                    .setParameter("account_id", account.getId())
+                    .getResultList();
+            return roleGroups.stream().flatMap(rg -> rg.getRoles().stream()).map(Enum::toString).collect(Collectors.toSet());
+        }
+        if (criteria instanceof Account) {
+            roleGroups = em.createNamedQuery("roleGroupsFromTeams", RoleGroup.class)
+                    .setParameter("account_id", account.getId())
+                    .getResultList();
+            return roleGroups.stream().flatMap(rg -> rg.getRoles().stream()).map(Enum::toString).collect(Collectors.toSet());
+        }
+        if (criteria instanceof Long) {
+            Team team = em.find(Team.class, criteria);
+            return roles(principal, team);
+        }
+        if (criteria instanceof String) {
+            String query = "SELECT t FROM Team t WHERE t.name = :name";
+            Team team = em.createQuery(query, Team.class).setParameter("name", criteria).getSingleResult();
+            return roles(principal, team);
+        }
+        return Collections.emptySet();
     }
 
 }

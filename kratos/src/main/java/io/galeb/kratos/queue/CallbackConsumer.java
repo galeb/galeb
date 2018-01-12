a7d2cb83ee91a8ceb69878b56107502fad36ed2f
Igor Fernandes <igor.fernandes@corp.globo.com>
2018-01-12 14:41:31 -0200

    Kratos: retrieve target information from repository before save healthStatus

--- kratos/src/main/java/io/galeb/kratos/queue/CallbackConsumer.java
+++ kratos/src/main/java/io/galeb/kratos/queue/CallbackConsumer.java
@@ -1,7 +1,9 @@
 package io.galeb.kratos.queue;
 
 import io.galeb.core.entity.HealthStatus;
+import io.galeb.core.entity.Target;
 import io.galeb.kratos.repository.HealthStatusRepository;
+import io.galeb.kratos.repository.TargetRepository;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,16 +18,21 @@ public class CallbackConsumer {
     private static final String QUEUE_HEALTH_CALLBACK = "healthstatus-callback";
 
     private final HealthStatusRepository healthStatusRepository;
+    private final TargetRepository targetRepository;
+
 
     @Autowired
-    public CallbackConsumer(HealthStatusRepository healthStatusRepository) {
+    public CallbackConsumer(HealthStatusRepository healthStatusRepository, TargetRepository targetRepository) {
         this.healthStatusRepository = healthStatusRepository;
+        this.targetRepository = targetRepository;
     }
 
     @JmsListener(destination = QUEUE_HEALTH_CALLBACK)
     public void callback(HealthStatus healthStatus) {
         try {
             if (healthStatus != null) {
+                Target target = targetRepository.findOne(healthStatus.getTarget().getId());
+                healthStatus.setTarget(target);
                 healthStatusRepository.save(healthStatus);
                 LOGGER.warn("HealthStatus [source: " + healthStatus.getSource() +  "] (from target " + healthStatus.getTarget().getName() + ") updated.");
             }

9d234cba66c9fb6d99becc2bb22b1062ea1a4cec
Marcelo Monteiro (tuxmonteiro) <marcelotmonteiro@gmail.com>
2018-01-15 18:18:20 -0200

    OldApi: Add info/healthcheck controllers

---
+++ oldapi/src/main/java/io/galeb/oldapi/controllers/HealthcheckController.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2014-2018 Globo.com - ATeam
+ * All rights reserved.
+ *
+ * This source is subject to the Apache License, Version 2.0.
+ * Please see the LICENSE file for more information.
+ *
+ * Authors: See AUTHORS file
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.galeb.oldapi.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@SuppressWarnings({"unused", "SameReturnValue"})
+@RestController
+public class HealthcheckController {
+
+    @GetMapping(value = "/healthcheck")
+    @ResponseBody
+    public String healthcheck() {
+        return "WORKING";
+    }
+}

d5fd7c383fe0bd1323aa031478cc290c2259b841
Marcelo Monteiro (tuxmonteiro) <marcelotmonteiro@gmail.com>
2018-01-15 18:32:53 -0200

    OldApi: add initial controllers and services

---
+++ oldapi/src/main/java/io/galeb/oldapi/controllers/ProjectController.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2014-2018 Globo.com - ATeam
+ * All rights reserved.
+ *
+ * This source is subject to the Apache License, Version 2.0.
+ * Please see the LICENSE file for more information.
+ *
+ * Authors: See AUTHORS file
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.galeb.oldapi.controllers;
+
+import io.galeb.oldapi.services.ProjectService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProjectController {
+
+    @Autowired
+    private ProjectService service;
+
+}

d5ea19eb8fd32917ec834b1c3ce47ce5e0a9cd8a
Tiago Machado <tiago.machado@corp.globo.com>
2018-01-19 14:25:24 -0200

    Added new tests for service RoutersService

--- legba/src/test/java/io/galeb/legba/services/RoutersServiceTest.java
+++ legba/src/test/java/io/galeb/legba/services/RoutersServiceTest.java
@@ -1,5 +1,8 @@
 package io.galeb.legba.services;
 
+import io.galeb.core.entity.Environment;
+import io.galeb.core.entity.Target;
+import io.galeb.core.services.ChangesService;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Date;
 import java.util.Set;
 
 @RunWith(SpringRunner.class)
@@ -15,6 +19,9 @@ public class RoutersServiceTest {
 
     @Autowired
     private RoutersService routersService;
+
+    @Autowired
+    private ChangesService changesService;
 
     @Test
     public void shouldRegisterRouter() {
@@ -36,5 +43,53 @@ public class RoutersServiceTest {
         boolean containsRouterAndVersion = groupID.getRouters().stream().anyMatch(r -> r.getLocalIp().equals(localIP) && r.getEtag().equals(version));
         Assert.assertTrue(containsRouterAndVersion);
 
+    }
+
+    @Test
+    public void shouldClearChangesWithNewestVersion() {
+        //Arrange
+        String groupId = "group-local";
+        String localIP = "127.0.0.1";
+        String versionOldest = "1";
+        String versionNewest = "2";
+        String envId = "1";
+
+        Environment env = new Environment();
+        env.setId(Long.valueOf(envId));
+        Target target = new Target();
+        target.setId(999L);
+        target.setLastModifiedAt(new Date());
+        changesService.register(env, target, versionOldest);
+
+        //Action
+        routersService.put(groupId, localIP, versionNewest, envId);
+
+        //Assert
+        boolean hasChanges = changesService.hasByEnvironmentId(Long.valueOf(envId));
+        Assert.assertFalse(hasChanges);
+    }
+
+    @Test
+    public void shouldNotClearChangesWithOldestVersion() {
+        //Arrange
+        String groupId = "group-local";
+        String localIP = "127.0.0.1";
+        String versionOldest = "1";
+        String versionNewest = "2";
+        String envId = "1";
+
+        Environment env = new Environment();
+        env.setId(Long.valueOf(envId));
+        Target target = new Target();
+        target.setId(999L);
+        target.setLastModifiedAt(new Date());
+        changesService.register(env, target, versionNewest);
+
+        //Action
+        routersService.put(groupId, localIP, versionOldest, envId);
+
+        //Assert
+        boolean hasChanges = changesService.hasByEnvironmentId(Long.valueOf(envId));
+        Assert.assertTrue(hasChanges);
     }
 }

78e25ed665e2671498211639cfd1733766034db6
Thaissa Falbo <thaissa.falbo@gmail.com>
2018-01-29 14:27:52 -0200

    adds swagger

---
+++ api/src/main/java/io/galeb/api/configuration/SwaggerConfiguration.java
@@ -0,0 +1,28 @@
+package io.galeb.api.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+@Import({springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration.class})
+public class SwaggerConfiguration {
+
+
+        @Bean
+        public Docket api() {
+            return new Docket(DocumentationType.SWAGGER_2)
+                    .select()
+                    .apis(RequestHandlerSelectors.any())
+                    .paths(PathSelectors.regex("/(tests)(/.*)*"))
+                    .build();
+        }
+}
+
+

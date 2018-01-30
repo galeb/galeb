482bcfebcf94900ac29d95f0cc4fcc3ed2117692
Tiago Machado <tiago.machado@corp.globo.com>
2018-01-30 18:41:47 -0200

    Remove unused imports

--- api/src/main/java/io/galeb/api/repository/AccountRepository.java
+++ api/src/main/java/io/galeb/api/repository/AccountRepository.java
@@ -21,7 +21,6 @@ import io.galeb.core.entity.Account;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.RepositoryDefinition;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.security.access.prepost.PreAuthorize;

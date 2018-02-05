7559cc2bf246bde3ff68a920e54153227f4cc109
Marcelo Monteiro (tuxmonteiro) <marcelotmonteiro@gmail.com>
2018-02-05 14:59:39 -0200

    OldAPI: Remove unnecessary "reason" param

--- oldapi/src/main/java/io/galeb/oldapi/exceptions/BadRequestException.java
+++ oldapi/src/main/java/io/galeb/oldapi/exceptions/BadRequestException.java
@@ -19,7 +19,7 @@ package io.galeb.oldapi.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value= HttpStatus.BAD_REQUEST, reason="Bad request")
+@ResponseStatus(value= HttpStatus.BAD_REQUEST)
 public class BadRequestException extends RuntimeException {
 
     private static final long serialVersionUID = 6986223937559244218L;

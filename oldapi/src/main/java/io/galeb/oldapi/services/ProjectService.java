17ef8a8ad9696e239d29deff52a4d79c20a7cff6
Igor Fernandes <igor.fernandes@corp.globo.com>
2018-02-19 17:40:28 -0300

    OldAPI: Refactoring post method

--- oldapi/src/main/java/io/galeb/oldapi/services/ProjectService.java
+++ oldapi/src/main/java/io/galeb/oldapi/services/ProjectService.java
@@ -20,6 +20,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import io.galeb.core.entity.Team;
 import io.galeb.oldapi.entities.v1.AbstractEntity;
 import io.galeb.oldapi.entities.v1.Project;
 import io.galeb.oldapi.services.http.Response;
@@ -65,27 +66,30 @@ public class ProjectService extends AbstractConverterService<Project> {
         return jsonNodeProject;
     }
 
+    private Set<Team> extracTeams(Set<String> teams) {
+        return teams.stream()
+                .map(teamUrl -> {
+                    try {
+                        long teamId = Long.parseLong(teamUrl.replaceAll("^.*/", ""));
+                        Team teamV2 = new Team();
+                        teamV2.setId(teamId);
+                        return teamV2;
+                    } catch (Exception ignored) { }
+                    return null;
+                })
+                .filter(Objects::nonNull)
+                .collect(Collectors.toSet());
+    }
+
     @Override
     public ResponseEntity<Resource<? extends AbstractEntity>> post(String body, Class<? extends io.galeb.core.entity.AbstractEntity> v2entityClass) {
         Project projectV1 = convertFromJsonStringToV1(body);
+
         if (projectV1 != null) {
             try {
                 io.galeb.core.entity.Project projectV2 = new io.galeb.core.entity.Project();
                 projectV2.setName(projectV1.getName());
-                Set<io.galeb.core.entity.Team> v2Teams = projectV1.getTeams().stream()
-                        .map(teamUrl -> {
-                            try {
-                                long teamId = Long.parseLong(teamUrl.replaceAll("^.*/", ""));
-                                io.galeb.core.entity.Team teamV2 = new io.galeb.core.entity.Team();
-                                teamV2.setId(teamId);
-                                return teamV2;
-                            } catch (Exception ignored) { }
-                            return null;
-                        })
-                        .filter(Objects::nonNull)
-                        .collect(Collectors.toSet());
-
-                projectV2.setTeams(v2Teams);
+                projectV2.setTeams(extracTeams(projectV1.getTeams()));
 
                 JsonNode jsonNode = rebuildProjectV2Json(projectV2);
                 String projectResourcePath = Project.class.getSimpleName().toLowerCase();

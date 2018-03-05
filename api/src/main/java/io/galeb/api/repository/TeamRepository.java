3307507b9abe221a9eea2d292fbdc2f3f9775ea3
thaissa.falbo <thaissa.falbo@gmail.com>
2018-03-05 16:01:27 -0300

    adds some find methods to api in order to help UI

--- api/src/main/java/io/galeb/api/repository/TeamRepository.java
+++ api/src/main/java/io/galeb/api/repository/TeamRepository.java
@@ -26,6 +26,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.security.access.prepost.PreAuthorize;
 
+
 @SuppressWarnings({"unused", "unchecked"})
 @RepositoryRestResource(path = "team", collectionResourceRel = "team", itemResourceRel = "team")
 public interface TeamRepository extends JpaRepository<Team, Long>, TeamRepositoryCustom {
@@ -49,4 +50,11 @@ public interface TeamRepository extends JpaRepository<Team, Long>, TeamRepositor
     @ExposeFilterSwagger
     @PreAuthorize("@perm.allowView(null , #this)")
     Page<Team> findAll(Pageable pageable);
+
+    Team findByName(@Param("name") String var1);
+
+    Iterable<Team> findByNameContaining(@Param("name") String name);
+
+//    Iterable<Team> findByNameContainingWithSize(@Param("name") String name, @Param("size") int size);
 }
+

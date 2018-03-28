fc4bd6bff4492f1849a210a9e6482e8ed98b7d81
Igor Fernandes <igor.fernandes@corp.globo.com>
2018-03-28 17:19:51 -0300

    Legba: missing AbstractController from previous commit

---
+++ legba/src/main/java/io/galeb/legba/controller/AbstractController.java
@@ -0,0 +1,19 @@
+package io.galeb.legba.controller;
+
+import io.galeb.legba.repository.EnvironmentRepository;
+import org.h2.util.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class AbstractController {
+
+    @Autowired
+    private EnvironmentRepository environmentRepository;
+
+    protected Long getEnvironmentId(String envname) {
+        if (StringUtils.isNumber(envname)) {
+            return Long.parseLong(envname);
+        } else {
+            return environmentRepository.findByNameIgnoreCase(envname).getId();
+        }
+    }
+}

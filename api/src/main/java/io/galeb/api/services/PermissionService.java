48f717d85da558e6878a8a9dfc86507937dd5e02
Tiago Machado <tiago.machado@corp.globo.com>
2018-06-27 11:37:12 -0300

    Renamed file PermissionService

--- api/src/main/java/io/galeb/api/services/PermissionService.java
+++ api/src/main/java/io/galeb/api/services/PermissionService.java
@@ -16,7 +16,6 @@
 
 package io.galeb.api.services;
 
-import io.galeb.api.repository.*;
 import io.galeb.api.repository.custom.WithRoles;
 import io.galeb.api.services.AuditService.AuditType;
 import io.galeb.core.entity.*;
@@ -30,16 +29,15 @@ import org.springframework.stereotype.Service;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.Collections;
-import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 @SuppressWarnings("WeakerAccess, unused")
-@Service("perm-old")
+@Service("perm")
 public class PermissionService {
 
     public enum Action {
-        SAVE,
+        CREATE,
+        UPDATE,
         DELETE,
         VIEW
     }
@@ -52,65 +50,73 @@ public class PermissionService {
 
     private static final Logger LOGGER = LogManager.getLogger(PermissionService.class);
 
-    public boolean allowSave(Object criteria, MethodSecurityExpressionOperations expressionOperations) {
+    private boolean allow(Action action, Object criteria, MethodSecurityExpressionOperations expressionOperations) {
         Account account = (Account)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        if (criteria instanceof Account) {
-            return isAdmin(account, Account.class.getSimpleName(), Action.SAVE.toString(), criteria) ||
-                    isMySelf(account, criteria, Account.class.getSimpleName(), Action.SAVE.toString());
+        if (LocalAdminService.NAME.equals(account.getUsername())) {
+            return true;
         }
-        return allow(account, criteria, expressionOperations, Action.SAVE.toString());
+
+        WithRoles repository;
+        if (expressionOperations.getThis() instanceof WithRoles) {
+            repository = (WithRoles) expressionOperations.getThis();
+        } else {
+            LOGGER.error("{} is not an instance of {}", expressionOperations.getThis(), WithRoles.class.getSimpleName());
+            return false;
+        }
+
+        Set<String> roles = repository.mergeAllRolesOf(account);
+
+        String classSimpleName = repository.classEntity().getSimpleName();
+        String roleUpperCase = classSimpleName.toUpperCase() + "_" + action;
+
+        boolean allow = false;
+        AuditType audiType = AuditType.ROLE;
+        boolean isAccount = criteria instanceof Account;
+        Set<String> contextRoles = repository.roles(criteria);
+        switch (action) {
+            case CREATE:
+                allow = isAccount ? ((Account) criteria).getId() == account.getId() : hasRoleSelf(roles, roleUpperCase);
+                break;
+            case UPDATE:
+            case DELETE:
+                allow = isAccount ? ((Account) criteria).getId() == account.getId() : hasContextRole(contextRoles, roleUpperCase);
+                break;
+            case VIEW:
+                allow = criteria == null || hasGlobal(criteria, repository.classEntity()) || hasContextRole(contextRoles, roleUpperCase);
+                break;
+
+        }
+        if (!allow) {
+            allow = hasRoleAll(roles, roleUpperCase);
+        }
+        auditService.logAccess(roleUpperCase, roles, allow, classSimpleName, action.toString(), criteria, audiType);
+        return allow;
+    }
+
+    public boolean allowSave(Object criteria, MethodSecurityExpressionOperations expressionOperations) {
+        Action action = Action.UPDATE;
+        if (criteria instanceof AbstractEntity) {
+            AbstractEntity entity = (AbstractEntity) criteria;
+            action = entity.getId() != 0 ? Action.UPDATE : Action.CREATE;
+        }
+
+        return allow(action, criteria, expressionOperations);
     }
 
     public boolean allowDelete(Object criteria, MethodSecurityExpressionOperations expressionOperations) {
-        Account account = (Account)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        return allow(account, criteria, expressionOperations, Action.DELETE.toString());
+        return allow(Action.DELETE, criteria, expressionOperations);
     }
 
     public boolean allowView(Object criteria, MethodSecurityExpressionOperations expressionOperations) {
-        Account account = (Account)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        Class<? extends AbstractEntity> entityClass = extractEntityClass(expressionOperations);
-        if (expressionOperations == null && criteria instanceof Account) {
-            entityClass = Account.class;
-            return isAdmin(account, entityClass.getSimpleName(), Action.VIEW.toString(), criteria) || account.getId() == ((Account) criteria).getId();
-        }
-        if (entityClass == null) return false;
-        if (criteria == null) {
-            String criteriaName = entityClass.getSimpleName();
-            String realCriteria = "ALL";
-            if (isAdmin(account, entityClass.getSimpleName(), Action.VIEW.toString(), realCriteria)) {
-                return true;
-            }
-            Set<String> roles = mergeRoles(account);
-            String roleView = criteriaName.toUpperCase() + "_VIEW";
-            boolean isView = roles.contains(roleView);
-            auditService.logAccess(roleView, roles, isView, criteriaName, Action.VIEW.toString(), realCriteria, AuditService.AuditType.ROLE);
-
-            String roleViewAll = roleView + "_ALL";
-            boolean isViewAll = roles.contains(roleViewAll);
-            auditService.logAccess(roleViewAll, roles, isViewAll, criteriaName, Action.VIEW.toString(), realCriteria, AuditService.AuditType.ROLE);
-            return true; // audit only
-        }
-        return hasGlobal(criteria, entityClass) || allow(account, criteria, expressionOperations, Action.VIEW.toString());
+        return allow(Action.VIEW, criteria, expressionOperations);
     }
 
-    @SuppressWarnings("unchecked")
-    private boolean allow(Account account, Object criteria, MethodSecurityExpressionOperations expressionOperations, String action) {
-        Class<? extends AbstractEntity> entityClass = extractEntityClass(expressionOperations);
-        if (entityClass == null) return false;
-        String roleEntityPrefix = entityClass.getSimpleName().toUpperCase();
-        String role = roleEntityPrefix + "_" + action;
-        final Object repositoryObj = expressionOperations.getThis();
-        WithRoles repository;
-        if (repositoryObj instanceof WithRoles) {
-            repository = (WithRoles) repositoryObj;
-        } else {
-            LOGGER.error("{} is not an instance of {}", repositoryObj, WithRoles.class.getSimpleName());
-            return false;
-        }
-        return (isAdmin(account, entityClass.getSimpleName(), action, criteria)) ||
-                       isMySelf(account, criteria, entityClass.getSimpleName(), action) ||
-                       (hasSelfRole(account, role + "_ALL", entityClass.getSimpleName(), action, criteria) ||
-                        hasContextRole(criteria, repository, role, entityClass.getSimpleName(), action));
+    private boolean hasRoleSelf(Set<String> roles, String role) {
+        return roles.stream().anyMatch(role::equals);
+    }
+
+    private boolean hasRoleAll(Set<String> roles, String role) {
+        return roles.stream().filter(r -> r.equals(role + "_ALL")).count() > 0;
     }
 
     private boolean hasGlobal(Object criteria, Class<? extends AbstractEntity> entityClass) {
@@ -125,88 +131,7 @@ public class PermissionService {
         return false;
     }
 
-    private boolean hasContextRole(Object criteria, Object repositoryObj, String role, String entityClass, String action) {
-        WithRoles repository;
-        if (repositoryObj instanceof WithRoles) {
-            repository = (WithRoles) repositoryObj;
-        } else {
-            LOGGER.error("{} is not an instance of {}", repositoryObj, WithRoles.class.getSimpleName());
-            return false;
-        }
-        Set<String> roles = repository.roles(criteria);
-        boolean result = roles.stream().anyMatch(role::equals);
-        auditService.logAccess(role, roles, result, entityClass, action, criteria, AuditType.ROLE);
-        return result;
+    private boolean hasContextRole(Set<String> roles, String role) {
+        return roles != null && roles.stream().anyMatch(role::equals);
     }
-
-    private boolean hasSelfRole(Account account, String role, String entityClass, String action, Object criteria) {
-        Set<String> roles = mergeRoles(account);
-        boolean result = roles.contains(role);
-        auditService.logAccess(role, roles, result, entityClass, action, criteria, AuditType.ROLE);
-        return result;
-    }
-
-    private Set<String> mergeRoles(Account account) {
-        long accountId = account.getId();
-        List<RoleGroup> roleGroupsFromTeams = em.createNamedQuery("roleGroupsFromTeams", RoleGroup.class)
-                .setParameter("id", accountId)
-                .getResultList();
-        Set<String> roles = roleGroupsFromTeams.stream().flatMap(rg -> rg.getRoles().stream())
-                .distinct().map(Enum::toString).distinct().collect(Collectors.toSet());
-        List<RoleGroup> roleGroupsFromAccount = em.createNamedQuery("roleGroupsFromAccount", RoleGroup.class)
-                .setParameter("id", accountId)
-                .getResultList();
-        roles.addAll(roleGroupsFromAccount.stream().flatMap(rg -> rg.getRoles().stream())
-                .distinct().map(Enum::toString).collect(Collectors.toSet()));
-        List<RoleGroup> roleGroupsFromProject = em.createNamedQuery("roleGroupsFromProjectByAccountId", RoleGroup.class)
-                .setParameter("id", accountId)
-                .getResultList();
-        roles.addAll(roleGroupsFromProject.stream().flatMap(rg -> rg.getRoles().stream())
-                .distinct().map(Enum::toString).collect(Collectors.toSet()));
-        return roles;
-    }
-
-    private boolean isAdmin(Account account, String entityClass, String action, Object criteria) {
-//        if (hasSelfRole(account, Role.ADMIN.toString(), entityClass, action, criteria)) {
-//            return true;
-//        }
-        boolean result = LocalAdminService.NAME.equals(account.getUsername());
-        auditService.logAccess(null, Collections.emptySet(), result, entityClass, action, criteria,  AuditType.LOCAL_ADMIN);
-        return result;
-    }
-
-    private boolean isMySelf(Account account, Object criteria, String entityClass, String action) {
-        boolean result = false;
-        if (criteria instanceof Account) {
-            result = criteria.equals(account);
-        }
-        if (criteria instanceof Long) {
-            Long id = (Long) criteria;
-            result = account.getId() == id;
-        }
-        auditService.logAccess(null, Collections.emptySet(), result, entityClass, action, criteria, AuditType.MYSELF);
-        return result;
-    }
-
-    private Class<? extends AbstractEntity> extractEntityClass(MethodSecurityExpressionOperations securityExpressionOperations) {
-        if (securityExpressionOperations == null) return null;
-        Object o = securityExpressionOperations.getThis();
-        // @formatter:off
-        return  o instanceof AccountRepository ? Account.class :
-                o instanceof BalancePolicyRepository ? BalancePolicy.class :
-                o instanceof EnvironmentRepository ? Environment.class :
-                o instanceof HealthCheckRepository ? HealthCheck.class :
-                o instanceof HealthStatusRepository ? HealthStatus.class :
-                o instanceof PoolRepository ? Pool.class :
-                o instanceof ProjectRepository ? Project.class :
-                o instanceof RoleGroupRepository ? RoleGroup.class :
-                o instanceof RuleOrderedRepository ? RuleOrdered.class :
-                o instanceof RuleRepository ? Rule.class :
-                o instanceof TargetRepository ? Target.class :
-                o instanceof TeamRepository ? Team.class :
-                o instanceof VirtualhostGroupRepository ? VirtualhostGroup.class :
-                o instanceof VirtualHostRepository ? VirtualHost.class : null;
-        // @formatter:on
-    }
-
 }

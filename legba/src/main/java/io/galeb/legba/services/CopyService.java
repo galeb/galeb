8e7e92fdb6713481ee5796169c88054037ad3613
Marcelo Monteiro (tuxmonteiro) <marcelotmonteiro@gmail.com>
2018-10-29 20:57:25 -0300

    Legba: Remove unnecessary entities pre-load

--- legba/src/main/java/io/galeb/legba/services/CopyService.java
+++ legba/src/main/java/io/galeb/legba/services/CopyService.java
@@ -16,16 +16,6 @@ public class CopyService {
     public List<VirtualHost> getVirtualHosts(Long envId) {
         List<VirtualHost> listVirtualHost;
         listVirtualHost = virtualHostRepository.findAllByEnvironmentId(envId);
-        listVirtualHost.stream().forEach(vh -> {
-            vh.getEnvironments();
-            vh.getVirtualhostgroup().getRulesordered().stream().forEach(ro -> ro.getRule().getPools().stream().forEach(p -> {
-                p.getBalancepolicy();
-                p.getHcHeaders();
-                p.getTargets().stream().forEach( t -> {
-                    t.getHealthStatus().stream().forEach(hs -> {});
-                });
-            }));
-        });
         return listVirtualHost;
     }
 

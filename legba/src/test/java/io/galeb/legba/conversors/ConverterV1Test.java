763d388915ad2fe31d925fcbdcd6331a16472065
Marcelo Monteiro (tuxmonteiro) <marcelotmonteiro@gmail.com>
2018-12-17 15:10:49 -0200

    Legba: Add suite tests support

---
+++ legba/src/test/java/io/galeb/legba/conversors/ConverterV1Test.java
@@ -0,0 +1,13 @@
+package io.galeb.legba.conversors;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ConverterV1Test {
+
+    @Test
+    public void voidTest() {
+        Assert.assertTrue(true);
+    }
+
+}
---
+++ legba/src/test/java/io/galeb/legba/suites/TestSuite.java
@@ -0,0 +1,13 @@
+package io.galeb.legba.suites;
+
+import io.galeb.legba.conversors.ConverterV1Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+    ConverterV1Test.class
+})
+public class TestSuite {
+
+}

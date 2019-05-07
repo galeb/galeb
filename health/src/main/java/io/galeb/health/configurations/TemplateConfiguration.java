03741d522ae51133f8a11563d20f865684b4874f
Marcelo Monteiro (tuxmonteiro) <marcelotmonteiro@gmail.com>
2019-05-07 19:57:55 -0300

    JMS HA: Fix client not using RR

--- health/src/main/java/io/galeb/health/configurations/TemplateConfiguration.java
+++ health/src/main/java/io/galeb/health/configurations/TemplateConfiguration.java
@@ -1,8 +1,20 @@
 package io.galeb.health.configurations;
 
 import io.galeb.core.enums.SystemEnv;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.apache.activemq.artemis.api.core.TransportConfiguration;
 import org.apache.activemq.artemis.api.core.client.loadbalance.RoundRobinConnectionLoadBalancingPolicy;
+import org.apache.activemq.artemis.core.remoting.impl.netty.NettyConnectorFactory;
+import org.apache.activemq.artemis.core.remoting.impl.netty.TransportConstants;
 import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory;
+import org.apache.activemq.artemis.utils.UUIDGenerator;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.connection.CachingConnectionFactory;
@@ -16,6 +28,8 @@ import static org.springframework.jms.support.destination.JmsDestinationAccessor
 @SuppressWarnings("Duplicates")
 @Configuration
 public class TemplateConfiguration {
+
+    private static final Logger LOGGER = LogManager.getLogger(TemplateConfiguration.class);
 
     private static final long   JMS_TIMEOUT = Long.parseLong(SystemEnv.JMS_TIMEOUT.getValue());
     private static final String BROKER_CONN = SystemEnv.BROKER_CONN.getValue();
@@ -40,7 +54,30 @@ public class TemplateConfiguration {
     @Bean(name="connectionFactory")
     public CachingConnectionFactory cachingConnectionFactory() throws JMSException {
         CachingConnectionFactory cachingConnectionFactory = new CachingConnectionFactory();
-        ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory(BROKER_CONN);
+        ActiveMQConnectionFactory connectionFactory;
+        List<TransportConfiguration> nettyConfigurations = new ArrayList<>();
+
+        if (BROKER_HA || BROKER_CONN.contains(",")) {
+            String[] uris = TransportConfiguration.splitHosts(BROKER_CONN);
+            Arrays.stream(uris).map(u -> URI.create(u).getHost()).forEach(host -> {
+                Map<String, Object> connectionParams = new HashMap<>();
+                Map<String, Object> extraParams = new HashMap<>();
+                extraParams.put("prefetchPolicy.all", 1);
+
+                Integer port = URI.create(uris[0]).getPort();
+                connectionParams.put(TransportConstants.HOST_PROP_NAME, host);
+                connectionParams.put(TransportConstants.PORT_PROP_NAME, port);
+                TransportConfiguration transportConfiguration = new TransportConfiguration(
+                    NettyConnectorFactory.class.getName(), connectionParams,
+                    UUIDGenerator.getInstance().generateStringUUID(), extraParams);
+                nettyConfigurations.add(transportConfiguration);
+                LOGGER.info(transportConfiguration.toJson());
+            });
+            connectionFactory = new ActiveMQConnectionFactory(true, nettyConfigurations.toArray(new TransportConfiguration[0]));
+        } else {
+            connectionFactory = new ActiveMQConnectionFactory(BROKER_CONN);
+        }
+
         connectionFactory.setUser(BROKER_USER);
         connectionFactory.setPassword(BROKER_PASS);
         connectionFactory.setBlockOnDurableSend(BROKER_BLOCKDURABLESEND);
--- kratos/src/main/java/io/galeb/kratos/configurations/JMSConfiguration.java
+++ kratos/src/main/java/io/galeb/kratos/configurations/JMSConfiguration.java
@@ -16,24 +16,37 @@
 
 package io.galeb.kratos.configurations;
 
+import static org.springframework.jms.support.destination.JmsDestinationAccessor.RECEIVE_TIMEOUT_NO_WAIT;
+
 import io.galeb.core.enums.SystemEnv;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.jms.ConnectionFactory;
+import javax.jms.JMSException;
+import org.apache.activemq.artemis.api.core.TransportConfiguration;
 import org.apache.activemq.artemis.api.core.client.loadbalance.RoundRobinConnectionLoadBalancingPolicy;
+import org.apache.activemq.artemis.core.remoting.impl.netty.NettyConnectorFactory;
+import org.apache.activemq.artemis.core.remoting.impl.netty.TransportConstants;
 import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory;
+import org.apache.activemq.artemis.utils.UUIDGenerator;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.annotation.EnableJms;
 import org.springframework.jms.connection.CachingConnectionFactory;
 import org.springframework.jms.core.JmsTemplate;
 
-import javax.jms.ConnectionFactory;
-import javax.jms.JMSException;
-
-import static org.springframework.jms.support.destination.JmsDestinationAccessor.RECEIVE_TIMEOUT_NO_WAIT;
-
 @SuppressWarnings("Duplicates")
 @Configuration
 @EnableJms
 public class JMSConfiguration {
+
+    private static final Logger LOGGER = LogManager.getLogger(JMSConfiguration.class);
 
     private static final long   JMS_TIMEOUT = Long.parseLong(SystemEnv.JMS_TIMEOUT.getValue());
     private static final String BROKER_CONN = SystemEnv.BROKER_CONN.getValue();
@@ -48,7 +61,30 @@ public class JMSConfiguration {
     @Bean(name="connectionFactory")
     public CachingConnectionFactory cachingConnectionFactory() throws JMSException {
         CachingConnectionFactory cachingConnectionFactory = new CachingConnectionFactory();
-        ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory(BROKER_CONN);
+        ActiveMQConnectionFactory connectionFactory;
+        List<TransportConfiguration> nettyConfigurations = new ArrayList<>();
+
+        if (BROKER_HA || BROKER_CONN.contains(",")) {
+            String[] uris = TransportConfiguration.splitHosts(BROKER_CONN);
+            Arrays.stream(uris).map(u -> URI.create(u).getHost()).forEach(host -> {
+                Map<String, Object> connectionParams = new HashMap<>();
+                Map<String, Object> extraParams = new HashMap<>();
+                extraParams.put("prefetchPolicy.all", 1);
+
+                Integer port = URI.create(uris[0]).getPort();
+                connectionParams.put(TransportConstants.HOST_PROP_NAME, host);
+                connectionParams.put(TransportConstants.PORT_PROP_NAME, port);
+                TransportConfiguration transportConfiguration = new TransportConfiguration(
+                    NettyConnectorFactory.class.getName(), connectionParams,
+                    UUIDGenerator.getInstance().generateStringUUID(), extraParams);
+                nettyConfigurations.add(transportConfiguration);
+                LOGGER.info(transportConfiguration.toJson());
+            });
+            connectionFactory = new ActiveMQConnectionFactory(true, nettyConfigurations.toArray(new TransportConfiguration[0]));
+        } else {
+            connectionFactory = new ActiveMQConnectionFactory(BROKER_CONN);
+        }
+
         connectionFactory.setUser(BROKER_USER);
         connectionFactory.setPassword(BROKER_PASS);
         connectionFactory.setBlockOnDurableSend(BROKER_BLOCKDURABLESEND);

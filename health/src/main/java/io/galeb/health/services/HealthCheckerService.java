b5e2ea38be538c82ed2a13c9aa03745128440f95
Marcelo Monteiro (tuxmonteiro) <marcelotmonteiro@gmail.com>
2019-05-08 13:10:54 -0300

    Healthcheck: Add SSL (with insecure TrustManager) support

--- health/src/main/java/io/galeb/health/services/HealthCheckerService.java
+++ health/src/main/java/io/galeb/health/services/HealthCheckerService.java
@@ -29,9 +29,22 @@ import io.galeb.core.log.JsonEventToLogger;
 import io.galeb.health.util.CallBackQueue;
 import io.netty.handler.codec.http.DefaultHttpHeaders;
 import io.netty.handler.codec.http.HttpHeaders;
+import io.netty.handler.codec.http2.Http2SecurityUtil;
+import io.netty.handler.ssl.ApplicationProtocolConfig;
+import io.netty.handler.ssl.ApplicationProtocolConfig.Protocol;
+import io.netty.handler.ssl.ApplicationProtocolConfig.SelectedListenerFailureBehavior;
+import io.netty.handler.ssl.ApplicationProtocolConfig.SelectorFailureBehavior;
+import io.netty.handler.ssl.ApplicationProtocolNames;
+import io.netty.handler.ssl.OpenSsl;
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslContextBuilder;
+import io.netty.handler.ssl.SslProvider;
+import io.netty.handler.ssl.SupportedCipherSuiteFilter;
+import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
 import java.util.Collections;
 import java.util.Map;
 import java.util.Optional;
+import javax.net.ssl.SSLException;
 import org.asynchttpclient.AsyncCompletionHandler;
 import org.asynchttpclient.AsyncHttpClient;
 import org.asynchttpclient.RequestBuilder;
@@ -68,7 +81,34 @@ public class HealthCheckerService {
                 .setConnectTimeout(connectionTimeout)
                 .setPooledConnectionIdleTimeout(pooledConnectionIdleTimeout)
                 .setMaxConnectionsPerHost(maxConnectionsPerHost)
+                .setSslContext(getSslContext())
                 .setUserAgent(HEALTHCHECKER_USERAGENT).build());
+    }
+
+    private SslContext getSslContext() {
+        try {
+            final SslProvider provider = OpenSsl.isAlpnSupported() ? SslProvider.OPENSSL : SslProvider.JDK;
+            return SslContextBuilder.forClient()
+                .sslProvider(provider)
+                /* NOTE: the cipher filter may not include all ciphers required by the HTTP/2 specification.
+                 * Please refer to the HTTP/2 specification for cipher requirements. */
+                .ciphers(Http2SecurityUtil.CIPHERS, SupportedCipherSuiteFilter.INSTANCE)
+                .trustManager(InsecureTrustManagerFactory.INSTANCE)
+                .applicationProtocolConfig(new ApplicationProtocolConfig(
+                    Protocol.ALPN,
+                    // NO_ADVERTISE is currently the only mode supported by both OpenSsl and JDK providers.
+                    SelectorFailureBehavior.NO_ADVERTISE,
+                    // ACCEPT is currently the only mode supported by both OpenSsl and JDK providers.
+                    SelectedListenerFailureBehavior.ACCEPT,
+                    ApplicationProtocolNames.HTTP_2,
+                    ApplicationProtocolNames.HTTP_1_1))
+                .build();
+        } catch (SSLException e) {
+            JsonEventToLogger errorEvent = new JsonEventToLogger(ROOT_CLASS);
+            errorEvent.put("short_message", "SslContext Error");
+            errorEvent.sendError(e);
+        }
+        return null;
     }
 
     @SuppressWarnings("unchecked")

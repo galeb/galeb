a72a51cfc9a94313836561c697d52527f2a5561e
isabella-coimbra <56848375+isabella-coimbra@users.noreply.github.com>
2020-10-20 12:22:08 -0300

    Remove useSSL

--- newcore/src/main/java/io/galeb/core/configuration/db/DatabaseConfiguration.java
+++ newcore/src/main/java/io/galeb/core/configuration/db/DatabaseConfiguration.java
@@ -74,7 +74,6 @@ public class DatabaseConfiguration {
         hikariDataSource.addDataSourceProperty("cacheServerConfiguration", DB_CACHE_SERVER_CONFIGURATION);
         hikariDataSource.addDataSourceProperty("elideSetAutoCommits", DB_ELIDE_SET_AUTO_COMMITS);
         hikariDataSource.addDataSourceProperty("maintainTimeStats", DB_MAINTAIN_TIME_STATS);
-        hikariDataSource.addDataSourceProperty("useSSL", "false");
 
         return hikariDataSource;
     }
